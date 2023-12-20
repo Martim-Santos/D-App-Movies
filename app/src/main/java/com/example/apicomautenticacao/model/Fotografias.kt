84103da7c781f7a7871bc3b497a75bcb1672549b
Martim-Santos <aluno24235@ipt.pt>
2023-12-20 15:19:37 +0000

    StartOfAPI

---
+++ app/src/main/java/com/example/apicomautenticacao/model/Fotografias.kt
@@ -0,0 +1,7 @@
+package com.example.apicomautenticacao.model
+
+import java.util.Date
+
+class Fotografias(
+    var ID: Int, var URL: String, var nome:String, var data: Date
+)
---
+++ app/src/main/java/com/example/apicomautenticacao/model/Utilizador.kt
@@ -0,0 +1,6 @@
+package com.example.apicomautenticacao.model
+
+class Utilizador(
+
+    var ID: Int, var Nome: String, var pass: String, var mail: String, var fotos: Array<String>
+)
